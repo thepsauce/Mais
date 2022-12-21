b775444bc5fbbfd5321b4dbb4e86a36457396905
MordorHD <53628409+MordorHD@users.noreply.github.com>
2022-12-21 15:33:16 +0400

    Update compilebase.h

--- src/compile/compilebase.h
+++ src/compile/compilebase.h
@@ -5,78 +5,93 @@ struct label;
 struct group;
 
 enum {
-    TOKEOF,
-    TOKNUMBER,
-    TOKSTRING,
-    TOKWORD,
-    TOKANY,
-    TOKKEYWORD,
+    TOKEOF, // end of file
+    TOKNUMBER, // 0x3123 0 123 8783 0b10010 0c2561
+    TOKSTRING, // "Hello there" "Banana"
+    TOKWORD, // word var func tree whatever
+    TOKANY, // . , ; : - > ( ) [ ] + * / % & | ^ = < > { } ! ? ~
+    TOKKEYWORD, // see list of keywords below
 };
 
 enum {
-	OPRLSH = 0x100,
-	OPRRSH,
-	OPRNOT
+	OPRLSH = 0x100, // <<
+	OPRRSH, // >>
+	OPRNOT // !!
 };
 
 struct token {
-    u32 type;
-    long pos;
-    union {
-        char word[1024];
-        struct {
+    u32 type; // type of token, check above for possible values
+    long pos; // position of this token inside the file (important for returning errors)
+    union { 
+        char word[1024]; // TOKWORD - we limit the maximum size of a word to 1024 bytes, anyone who goes over this limit is just crazy
+        struct { // TOKSTRING - variable length, this go on a little longer
             char *str;
             u32 strLen;
         };
-        i64 l;
-        int c;
+        i64 l; // TOKNUMBER
+        int c; // TOKANY
     };
 };
 
 struct parser {
 	// console information
-#ifdef __WIN32
+#ifdef __WIN32 // just stuff for windows to produce colored text output
 	CONSOLE_SCREEN_BUFFER_INFO csbiInfo;
 	HANDLE hOut;
 #else
 
 #endif
-
-	File file;
-	char *fileName;
-	struct token tok;
-	struct token unTok;
-	char buf[1024];
-	int nBuf, iBuf;
-	long fOff;
-	bool stayOnToken; // if this is true, psreadtok doesn't do anything
+	File file; // the file we are working on
+	char *fileName; // the name of that file
+	struct token tok; // current token
+	char buf[1024]; // io file buffer
+	int nBuf; // right index of io cursor
+	int iBuf; // left index of io cursor
+	long fOff; // offset of buf inside the actual file
+	bool stayOnToken; // if this is true, psreadtok doesn't do anything (set on a previous call
 	int lastTokRet; // last return value of 'pspeektok'
 	bool failOnEof; // if this is true and psreadtok would return eof, an error is returned
 	
-	int *scopes;
-	int scopeCnt, scopeCap;
-	struct label *labels;
-	int labelCnt, labelCap;
+	int *scopes; // this array stores indexes in the labels array, this way, we only need to keep one label array
+	int scopeCnt, scopeCap; // count and capacity of scope array
+	struct label *labels; // this array stores all labels like variables, functions, typedefs and gotos
+	int labelCnt, labelCap;  // count and capacity of label array
 };
 
+// initializes the parser struct
 int psinit(struct parser *parser);
+// frees all resources associated to this parser struct, note that this doesn't close the file stream
 int psdiscard(struct parser *parser);
+// opens a file
 int psopen(struct parser *parser, const char *file);
+// closes last opened file
 int psclose(struct parser *parser);
+// returns the next character in the buffer but without moving the cursor
+// or EOF if the end of the file has been reached
 int pspeek(struct parser *parser);
+// moves the cursor to the next position inside the buffer
 int psskip(struct parser *parser);
+// returns the next character inside the buffer and moves the cursor one over
+// or EOF if the end of the file has been reached
 int psread(struct parser *parser);
+// returns the current position of the cursor inside the file
 long pstell(struct parser *parser);
+// reads in the next token and returns 0 on success or EOF
 int psreadtok(struct parser *parser);
+// reads in the next token and keeps it for another call to psreadtok (it is kind of a fake peek)
 int pspeektok(struct parser *parser);
+// throws an error
 int psthrow(struct parser *parser, const char *format, ...);
+// thors a warning
 int pswarn(struct parser *parser, const char *format, ...);
+// adds a new label to this parser, if push is true, this label is also pushed to the scope stack
 int psaddlabel(struct parser *parser, const struct label *label, bool push);
+// adds a new group to this parser
 int psaddgroup(struct parser *parser, const struct group *group);
 
 const char *Keywords[] = {
     "db", "dw", "dd", "dq", "dt", "dr", "df",
-    "typedef", "local", "readonly", "times", "this", "sizeof", // 13
+    "typedef", "local", "readonly", "times", "this", "sizeof",
     "exit", "test", "jmp", "jz", "jnz", "je", "jne", "jl", "jle", "jg", "jge",
     "ret", "re", "rne", "rl", "rle", "rg", "rge",
     "inc", "dec", "not", "and", "or", "xor", "lsh", "rsh",
@@ -87,6 +102,7 @@ const char *Keywords[] = {
 
 #define tokischar(tok, _c) (((tok)->type==TOKANY)&&(tok)->c==_c)
 
+// will add comments to the following later...
 #define DATATIMES 1
 #define DATANUMBER 2
 #define DATANOC 3
