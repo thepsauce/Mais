f6a89ab6334feea9c627a7e0c07c435b2b72c06f
MordorHD <53628409+MordorHD@users.noreply.github.com>
2022-12-22 18:40:34 +0400

    added template for expression

---
+++ src/compile/expr.h
@@ -0,0 +1,20 @@
+// parses an expression
+
+// examples of input:
+// 1 + 1
+// 2 * a
+// 44 / b % k
+// 3 * (4 + 9)
+// a * (31 - 33)
+// a[b + 1]
+// [a]
+// ?
+// 41 * (31 + 42 * (22 - 3))
+// f(x) * 33 * f(f(x))
+
+int parseexpr(struct parser *parser, struct group *group)
+{
+
+
+	return 0;
+}
