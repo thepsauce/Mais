cc36b4f962f557ffae635ff2a26cf014ba8039b5
MordorHD <53628409+MordorHD@users.noreply.github.com>
2022-12-22 18:38:48 +0400

    added type.h

---
+++ type.h
@@ -0,0 +1,17 @@
+// parses a type
+
+// examples of input:
+// db
+// [dd]
+// dt Class
+// dt Struct
+// df(p1: db ?, p2: db ? ) dd
+// dt Class<dd>
+// [dt Class<dt Class<dd>>]
+
+int parsedata(struct parser *parser, struct type *type)
+{
+
+
+	return 0;
+}
