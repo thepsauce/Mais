57c5484567c8e5ae7c81c652cb5d7aa1bc467302
MordorHD <53628409+MordorHD@users.noreply.github.com>
2022-12-22 21:17:57 +0400

    added keyword templates

---
+++ src/compile/keyword.h
@@ -0,0 +1,224 @@
+int parsetest(struct parser *parser)
+{
+	
+	return 0;
+}
+
+int parsejmp(struct parser *parser)
+{
+	
+	return 0;
+}
+
+int parsejz(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsejnz(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseje(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsejne(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsejl(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsejle(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsejg(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsejge(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseret(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsere(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parserne(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parserl(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parserg(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parserge(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseinc(struct parser *parser)
+{
+	return 0;
+}
+
+int parsedec(struct parser *parser)
+{
+	return 0;
+}
+
+int parsenot(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseand(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseor(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsexor(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parselsh(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsersh(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseadd(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsesub(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsemul(struct parser *parser)
+{
+	return 0;
+}
+
+int parsediv(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsemod(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parseneg(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsefadd(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsefsub(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsefmul(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsefdiv(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsefmod(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsefneg(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsemov(struct parser *parser)
+{
+
+	return 0;
+}
+
+int parsecall(struct parser *parser)
+{
+
+	return 0;
+}
