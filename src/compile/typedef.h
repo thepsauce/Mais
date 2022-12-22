8c2f7bc785053ac4ad35c9ba10f2467de4a60a3e
MordorHD <53628409+MordorHD@users.noreply.github.com>
2022-12-22 21:17:12 +0400

    Add files via upload

---
+++ src/compile/typedef.h
@@ -0,0 +1,14 @@
+// parse a typedef
+
+// examples:
+// typedef A
+// typedef [B]
+// typdef C<M>
+// typedef D : A
+// typedef E : C<dd>
+// typedef F<N> : C< <N> >
+int parsetypedef(struct parser *parser, struct label *label)
+{
+
+	return 0;
+}
---
+++ src/compile/varfuncgoto.h
@@ -0,0 +1,12 @@
+// parse a variable, function or goto
+
+// variable: [name]: [attributes] [type] [value] (value can be optional depending on the type)
+// function: [name]: [name]([params]) [type] 
+// goto: [name]: 
+
+int parsevarfuncgoto(struct parser *parser, struct label *label)
+{
+
+
+	return 0;
+}
